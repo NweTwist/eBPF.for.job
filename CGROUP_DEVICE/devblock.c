c00a9ac7b6f9fae315fb193f69841c2212202427
Cope.2 <125507076+NweTwist@users.noreply.github.com>
2025-12-10 12:21:04 +0300

    Commit 1 (device test)

---
+++ CGROUP_DEVICE/devblock.c
@@ -0,0 +1,44 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <linux/errno.h>
+
+char _license[] SEC("license") = "GPL";
+
+static __always_inline bool is_block_dev(__u32 access_type){
+    return access_type & BPF_DEVCG_DEV_BLOCK;
+}
+
+static __always_inline bool is_char_dev (__u32 access_type){
+    return access_type & BPF_DEVCG_DEV_CHAR;
+}
+
+static __always_inline bool is_rwm(__u32 access_type){
+    return access_type & (BPF_DEVCG_ACC_READ | BPF_DEVCG_ACC_WRITE | BPF_DEVCG_ACC_MKNOD);
+}
+
+SEC ("cgroup/dev")
+
+int devblock (struct bpf_cgroup_dev_ctx *ctx)
+{
+
+#ifdef TEST_MODE
+    if (is_char_dev(ctx->access_type) && is_rwm(ctx->access_type)) {
+
+        if (ctx->major == 1 && ctx->minor == 3){
+            return -EPERM;
+        }
+    }
+    
+    return 0;
+
+#else
+    if (is_block_dev(ctx->access_type) && is_rwm(ctx->access_type)){
+        if(ctx->major==0 || (ctx->major >= 65 && ctx->major <= 71) || ctx->major == 259) {
+            return -EPERM; 
+        }
+    }
+    return 0;
+
+#endif
+}
+
